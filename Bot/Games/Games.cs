b0b597c446026abe8201cc1b8073e71c9d8de319
Adrian <javid1541@gmail.com>
2022-10-05 18:35:14 -0500

    edit

--- Bot/Games/Games.cs
+++ Bot/Games/Games.cs
@@ -1,9 +1,13 @@
 // This namespace is meant to house multiple games. (Only one game is implemented).
 namespace Games {
-  // (only the Main game is implemented)
+  // (only the main game is implemented)
   public enum GameType : byte {
+    // This is a bare-bones "game" where players can move a character around a 2D grid.
     Main,
+
+    // I didn't implement Chess.
     Chess
+
     // (other games would be listed here had there been more)
   }
 }
